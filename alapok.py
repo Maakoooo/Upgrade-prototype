675686a3b839a682095deb1f3383d04b49b823fb
Maakoooo <ragomarci29@gmail.com>
2021-05-19 20:23:25 +0200

    indoklások, leírások, letisztázás

--- alapok.py
+++ alapok.py
@@ -1,12 +1,15 @@
 import random as rnd
 
-#feladat: fejlesztés - prototípus
-
 #tier 1 upgrade cost: 20, upgrade multiplier: 1.25, drop chance: 50%
 #tier 2 Upgrade cost: 40 upgrade multiplier: 2.5, drop chance: 30%
 #tier 3 upgrade cost: 60 upgrade multiplier: 5.0, drop chance: 5%
+#minden itemnek van egy száma és minden számhoz tartozik még:
+#az item jelenlegi szintje - akkor tér el egytől ha már birtokoljuk és fejlesztettük is
+#item alapára - ezt fogjuk szorozni mindig, amikor fejlesztjük
+#item alap buff-ja - ezt fogjuk szorozni, amikor fejlesztjük
+#a tiert az item sorszáma, a szorzóját a tiere határozza meg
 
-def Upgrade(): #gomb lenyomásakor haíjuk meg
+def upgrade(): #gomb lenyomásakor hívjuk meg
     pass
 
 def dropItem():
@@ -15,25 +18,19 @@ def dropItem():
         if (dropResult <= dropChance[2]):
             if (dropResult <= dropChance[1]):
                 if (dropResult <= dropChance[0]):
-                    whichTier = 3
+                    ownedItems.append(rnd.randInt(20,30)) #tier 3-as itemek sorszámai: 21-30
                 else:
-                    whichTier = 2
+                    ownedItems.append(rnd.randInt(10,20)) #tier 2-es itemek sorszámai: 11-20
             else:
-                whichTier = 1
+                ownedItems.append(rnd.randInt(0,10)) #tier 1-es itemek sorszámai: 1-10
         else:
-            whichTier = 0 #nem kapunk itemet
+            #nem kapunk itemet
 
 def addMoney():
     money += 30
 
-whichTier = 0
-dropChance = [5, 35, 85]
+dropChance = [5, 35, 85] #az első szám a tier 3 drop chance-e, a második a tier 2 és tier 3 összege, a harmadik az összes tier drop chance-e összeadva
 dropResult = 0
 bossAlive = True
 money = 0
-itemLevels = {
-    "damage":1,
-    "speed":1,
-    "luck":1
-}
-ownedItems = []
+ownedItems = []  
