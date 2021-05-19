cf75a97b7c6a74ad921b7f1e20567453d9f8ef96
Maakoooo <ragomarci29@gmail.com>
2021-05-19 17:27:34 +0200

    Droppolás esélyének és eredményének kiszámítása.

--- test.py
+++ test.py
@@ -2,17 +2,35 @@ import random as rnd
 
 #feladat: fejlesztés - prototípus
 
-#tier 1 upgrade cost: 20, upgrade multiplier: 1.25, drop chance: 60%
-#tier 2 Upgrade cost: 40 upgrade multiplier: 2.5, drop chance: 35%
+#tier 1 upgrade cost: 20, upgrade multiplier: 1.25, drop chance: 50%
+#tier 2 Upgrade cost: 40 upgrade multiplier: 2.5, drop chance: 30%
 #tier 3 upgrade cost: 60 upgrade multiplier: 5.0, drop chance: 5%
 
+def Upgrade(): #gomb lenyomásakor haíjuk meg
+    pass
 
 def dropItem():
-    pass
+    if (bossalive = False): #ezt az ifet kivesszük, ha készen lesz az a függvény, ami a boss halálakor meghívja ezt a függvényt
+        dropResult = rnd.randInt(0,100)
+        if (dropResult <= dropChance[2]):
+            if (dropResult <= dropChance[1]):
+                if (dropResult <= dropChance[0]):
+                    whichTier = 3
+                else:
+                    whichTier = 2
+            else:
+                whichTier = 1
+        else:
+            whichTier = 0 #nem kapunk itemet
+
 
 def addMoney():
     money += 30
 
+whichTier = 0
+dropChance = [5, 35, 85]
+dropResult = 0
+bossAlive = True
 money = 0
 itemLevels = {
     "damage":1,
